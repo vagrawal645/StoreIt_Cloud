019831adb5dbcf9af92bd6bf9beb9d84a3bbc3fc
vagrawal645 <43231728+vagrawal645@users.noreply.github.com>
2019-10-10 22:03:57 +0530

    Add files via upload

---
+++ models/User.js
@@ -0,0 +1,8 @@
+const Mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+var UserSchema = new Mongoose.Schema({
+    username : String,
+    password : String
+});
+UserSchema.plugin(passportLocalMongoose);
+module.exports = Mongoose.model("User",UserSchema);
