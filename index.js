e57b0a7eeda8bb58964a7fddbd39766c0234e58e
iamssh <saadhasan620@gmail.com>
2019-10-14 12:14:39 +0530

    update required in up/id and delete route

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const User = require("./models/User");
 const Grid = require('gridfs-stream');
 var flash = require('connect-flash');
 const mongoose = require('mongoose');
+var passport = require('passport');
 const express = require('express');
 const multer = require('multer');
-var passport = require('passport');
 const app = express();
 
 // Middleware
@@ -26,87 +26,104 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 app.use( express.static( "public" ) );
+app.use(function(req,res,next){
+    res.locals.currUser = req.user;
+    next();
+})
 
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-
-const mongoURI = 'mongodb://amdin:adminamdin1@ds217078.mlab.com:17078/use-me-cloud';
+const mongoURI = 'DATABASE_URL';
 mongoose.connect(mongoURI);
 const conn = mongoose.createConnection(mongoURI);
+
 let gfs;
+let loggedInUser;
 
 conn.once('open', () => {
   // Init stream
   gfs = Grid(conn.db, mongoose.mongo);
   gfs.collection('uploads');
 });
-
-app.get('/',function(req,res) {
+app.get('/',function(req,res){
   res.render('home');
-});
-
+})
 //Auth Routes
-
-app.get('/register',function(req,res) {  
+app.get('/register',function(req,res){
   res.render('register');
-});
+})
+app.post('/register',function(req,res){
 
-app.post('/register',function(req,res) {
-  
-  // Create a collection in the database with same name as the username
-
-  
-  
   User.register(new User({username:req.body.username}),req.body.password,function(err,user){
     if(err){
       console.log(err);
       return res.render('register');
     }
     passport.authenticate('local')(req,res,function(){      
-        res.redirect('/up');
+        res.redirect('/up/'+req.body.username);
     })
    })
-});
-
-app.get('/login',function(req,res){
-  res.render('login', {message : req.flash("info"), mess : req.flash("err")});
-});
-
+ });
+ app.get('/login',function(req,res){
+  res.render('login',{message : req.flash("info"),mess : req.flash("err")});
+})
 app.post('/login',
-  passport.authenticate('local', 
-  { successRedirect: '/up',  failureRedirect: '/login' }),
-  function(req,res){
-    //req.flash("err","Invalid username/password")
-  }
-);
+function(req,res){
+  passport.authenticate('local')(req,res,function(){   
+    loggedInUser = req.body.username;
+    res.redirect('/up/'+req.body.username);
+  })
+}
 
+);
 app.get('/logout',function(req,res){
   req.logout();
   res.redirect("/");
-});
+})
 
-app.get('/up',isLoggedIn, (req, res) => {
-  gfs.files.find().toArray((err, files) => {
-    // Check if files
-    if (!files || files.length === 0) {
-      res.render('index', { files: false });
-    } else {
-      res.render('index', { files: files });
+app.get('/up/:id', isLoggedIn, (req, res) => {
+  User.findOne({username : req.params.id}, function(err,user){
+    if (user !== null)
+    {
+      loggedInUser = user;
     }
+    
+    if(err) res.json(err);
+    else{      
+      if(user == null){
+        return res.render('index', { files: false });
+      }
+
+      var userFilesArray = user.file;
+      
+      res.render('index', {files: userFilesArray});
+      
+      // Functions to be added here :-
+
+      /* 
+        1. The User object is stored in the variable loggedInUser.
+        2. Check the length of 'file' array of the user, e.g. if (loggedInUser.file.length !== 0).
+        3. If there are files in the array, we nwwd to fetch the files. Search for the files in 
+            uploads collection with the name of the file that will be stored in the 'file' array itself.
+        4. Pass the files to 'index' page to display
+      */
+
+
+    };
   });
 });
+//Auth Routes end
 
-// @route GET /
-// @desc Loads form
+
 // @route POST /upload
 // @desc  Uploads file to DB
-
 // ************    Insert authentication middleware in this route!   ************
 app.post('/upload', (req, res) => {
   const storage = new GridFsStorage({
     url: mongoURI,
     file: (req, file) => {
+      fileName = file.originalname;
       return  { filename: file.originalname, bucketName: 'uploads' };
     }
   });
@@ -114,13 +131,19 @@ app.post('/upload', (req, res) => {
   let upload = multer({
     storage: storage
   }).single('file');
-
+  
+  
   upload(req,res, (err) => {
     if(err){
          res.json({error_code:1,err_desc:err});
          return;
     }
-    res.redirect('/up');
+    var newFile = req.file.filename;
+    var q = newFile.split(' ').join('_');
+    loggedInUser.file.push(q);
+    User.findByIdAndUpdate({_id: loggedInUser.id}, {$set: {file: loggedInUser.file}}).then((updatedDoc) => {})
+    console.log(q);
+    res.redirect('/up/' + loggedInUser.username);
   });
   
 });
@@ -187,11 +210,18 @@ app.delete('/files/:id', (req, res) => {
     if (err) {
       return res.status(404).json({ err: err });
     }
+    
+    
+    
+    // 1. Delete the filename from user's file array and,
 
-    res.redirect('/up');
+    // 2. update it in the database
+    
+    
+    
+    res.redirect('/up/' + loggedInUser.username);
   });
 });
-
 function isLoggedIn(req,res,next){
     if(req.isAuthenticated()){
       return next();
@@ -202,6 +232,6 @@ function isLoggedIn(req,res,next){
 
     }
 }
-
 const port = 8080;
+
 app.listen(port, () => console.log(`Server started on port ${port}`));
--- models/User.js
+++ models/User.js
@@ -2,7 +2,8 @@ const Mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 var UserSchema = new Mongoose.Schema({
     username : String,
-    password : String
+    password : String,
+    file : []
 });
 UserSchema.plugin(passportLocalMongoose);
 module.exports = Mongoose.model("User",UserSchema);
